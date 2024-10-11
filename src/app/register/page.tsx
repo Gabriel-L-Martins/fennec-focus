fa434d5c9c3c38141b7bae259aa1e40eb2b453f6
Gabriel <gbielacerda.m@gmail.com>
2024-10-11 15:06:24 -0300

    Remove unused lucide icon import

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Home, Lock, Mail, User } from 'lucide-react';
+import { Lock, Mail, User } from 'lucide-react';
 import React, { useState } from 'react';
 import { supabase } from '../../../supabaseClient';
 
