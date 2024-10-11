0f5526ce7d5b3e0ce6c0a1be8f2e00aefda2db2a
Gabriel <gbielacerda.m@gmail.com>
2024-10-11 08:13:45 -0300

    Added landing page

---
+++ src/app/landingPage.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './components/navbar';
+
+const LandingPage: React.FC = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className='text-primary-content flex justify-between items-center px-16'>
+                <div className='flex flex-col space-y-12'>
+                    <h1 className="max-w-xl text-6xl font-bold leading-tight">
+                        Master Your Studies Using {''}
+                        <span className='bg-gradient-to-r from-accent to-secondary gradient-text'>
+                            Fennec Focus {''}
+                        </span>
+                        with Ease
+                    </h1>
+                    <div>
+                        <button className='btn btn-lg btn-wide btn-primary'>
+                            Start Now!
+                        </button>
+                    </div>
+                </div>
+                <img src="https://placehold.co/400x600" className='rounded-lg invert' />
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
