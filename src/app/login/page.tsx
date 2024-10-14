572b7955f24a07dccb6d5cc285c9043f866532fb
Gabriel <gbielacerda.m@gmail.com>
2024-10-14 18:27:58 -0300

    Removed auth related code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,12 +2,10 @@
 
 import { Lock, Mail } from 'lucide-react';
 import React, { useState } from 'react';
-import { supabase } from '../../../supabaseClient';
 
 const Login: React.FC = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
 
     const [passwordVisible, setPasswordVisible] = useState(false);
 
@@ -15,27 +13,13 @@ const Login: React.FC = () => {
         setPasswordVisible(!passwordVisible);
     };
 
-    const handleLogin = async (e: React.FormEvent) => {
-        e.preventDefault();
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email,
-            password
-        });        
-        if (error) {
-            setError(error.message);
-        } else {
-            console.log('User logged in:', data.user);
-        }
-    };
-
     return (
         <div className='min-h-screen flex justify-center items-center text-primary-content '>
             <div className='min-w-80 bg-base-100 py-6 px-4 rounded-xl border border-primary-content'>
-                <form className='flex flex-col gap-4' onSubmit={handleLogin}>
+                <form className='flex flex-col gap-4'>
                     <div className='flex flex-col gap-1 mb-2'>
                         <h2 className='text-4xl font-semibold'>Welcome Back!</h2>
                         <p className='text-lg'>Sign-in back to your account.</p>
-                        {error && <p className='text-red-500'>{error}</p>}
                     </div>
                     <label className="input input-bordered flex justify-between items-center text-secondary-content">
                         <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,13 +2,11 @@
 
 import { Lock, Mail, User } from 'lucide-react';
 import React, { useState } from 'react';
-import { supabase } from '../../../supabaseClient';
 
 const Register: React.FC = () => {
     const [fullName, setFullName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
 
     const [passwordVisible, setPasswordVisible] = useState(false);
 
@@ -16,28 +14,13 @@ const Register: React.FC = () => {
         setPasswordVisible(!passwordVisible);
     };
 
-    const handleRegister = async (e: React.FormEvent) => {
-        e.preventDefault();
-        const { data, error } = await supabase.auth.signUp({
-            email,
-            password
-        });
-        if (error) {
-            setError(error.message);
-        } else {
-            console.log('User registered:', data.user);
-        }
-        
-    };
-
     return (
         <div className='min-h-screen flex justify-center items-center text-primary-content '>
             <div className='min-w-80 bg-base-100 py-6 px-4 rounded-xl border border-primary-content'>
-                <form className='flex flex-col gap-4' onSubmit={handleRegister}>
+                <form className='flex flex-col gap-4'>
                     <div className='flex flex-col gap-1 mb-2'>
                         <h2 className='text-4xl font-semibold'>Get Started</h2>
                         <p className='text-lg'>Create a new account.</p>
-                        {error && <p className='text-red-500'>{error}</p>}
                     </div>
                     <label className="input input-bordered flex justify-between items-center text-secondary-content">
                         <input type="text" placeholder="Full Name" value={fullName} onChange={(e) => setFullName(e.target.value)} />
